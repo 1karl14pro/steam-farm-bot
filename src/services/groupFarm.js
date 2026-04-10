b7c477fd2943de1deb15003e0d20698dfbe4c9dd
1karl14pro <1karl14pro@gmail.com>
2026-04-10 15:09:18 +0500

    Исправлен список бесплатных игр - убраны платные (Rust, GTA, Palworld)

--- src/services/groupFarm.js
+++ src/services/groupFarm.js
@@ -3,13 +3,13 @@ export const FREE_GAMES = [
   { appId: 730, name: 'Counter-Strike 2' },
   { appId: 570, name: 'Dota 2' },
   { appId: 440, name: 'Team Fortress 2' },
-  { appId: 252490, name: 'Rust' },
   { appId: 578080, name: 'PUBG: BATTLEGROUNDS' },
   { appId: 1172470, name: 'Apex Legends' },
-  { appId: 1938090, name: 'Call of Duty®: Warzone™' },
-  { appId: 359550, name: 'Rainbow Six Siege' },
-  { appId: 271590, name: 'Grand Theft Auto V' },
-  { appId: 1623730, name: 'Palworld' }
+  { appId: 1938090, name: 'Call of Duty: Warzone' },
+  { appId: 1517290, name: 'Battlefield 2042' },
+  { appId: 1203220, name: 'NARAKA: BLADEPOINT' },
+  { appId: 2357570, name: 'SpellForce: Conquest of Eo' },
+  { appId: 1599340, name: 'Lost Ark' }
 ];
 
 // Получить список бесплатных игр в зависимости от лимита
